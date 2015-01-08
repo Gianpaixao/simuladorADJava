751d5d22aae341ff3596a1bc245d2015bf7fab55
Gian <gian.paixao@gmail.com>
2015-01-08 20:16:47 -0200

    Criei um arquivo que gera tempos exponenciais

---
+++ SimuladorVirus/src/Gerador.java
@@ -0,0 +1,39 @@
+import java.util.Random;
+
+public class Gerador {
+
+	private static Gerador geradorAleatorio;
+	private Random random;
+
+	public Gerador() {
+		
+		Random rand = new Random();
+		long semente = rand.nextLong();
+		
+		random = new Random(semente);
+	}
+
+	public Gerador getGeradorAleatorio() {
+		
+		if (geradorAleatorio == null) {
+			geradorAleatorio = new Gerador();
+		}
+		
+		return geradorAleatorio;
+	}
+	
+	public double getGeraAmostra(double taxa) {
+		
+		return -(Math.log(random.nextDouble()) / taxa);
+	}
+	
+	public double geradorExponencial(double taxa)
+	{
+		Random rand = new Random();
+		double semente = rand.nextDouble();
+		return -(Math.log(1-semente)/taxa);
+	}
+
+	
+}
+
--- SimuladorVirus/src/Simulador.java
+++ SimuladorVirus/src/Simulador.java
@@ -1,8 +1,11 @@
+import java.io.File;
+import java.io.IOException;
 import java.util.Random;
 import java.util.Scanner;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
+import org.jfree.chart.ChartUtilities;
 import org.jfree.chart.JFreeChart;
 import org.jfree.chart.plot.PlotOrientation;
 import org.jfree.data.xy.XYSeries;
@@ -11,16 +14,18 @@ import org.jfree.ui.ApplicationFrame;
 import org.jfree.ui.RefineryUtilities;
 
 
+
 public class Simulador extends ApplicationFrame{
+	static Gerador gerador = new Gerador();
 	final static XYSeries series  = new XYSeries ("custo médio");
 	final static XYSeries seriesPi0  = new XYSeries ("PI0");
 	final static XYSeries seriesCs  = new XYSeries ("CS");
 	final static XYSeries seriesCv  = new XYSeries ("CV");
 	static double r1, r2, r3, r4, lambda, pi_0, pi_p, pi_r, pi_f;
-	static double media = 0.0, variancia = 0.0, desvio, confiancaMenor, confiancaMaior;
+	static double media = 0.0, variancia = 0.0, desvio, mediaCv = 0.0, mediaCs = 0.0, confiancaMenor, confiancaMaior;
 	static char estado = '0';
 	static int i, j, k, qtde, random, amostra = 50, iteracoes = 50;
-	static double t_0 = 0.0, t_p = 0.0, t_r = 0.0, t_f = 0.0, tempo = 0.0, cv, cs, custo[] = new double[amostra];
+	static double t_0 = 0.0, t_p = 0.0, t_r = 0.0, t_f = 0.0, tempo = 0.0, cv, cs, custo[] = new double[amostra],custoNo[] = new double[amostra],custoAmostra[] = new double[amostra];
 	
 	public Simulador(XYSeries series, String nomeGrafico, String variavel) {
 		super("Grafico de Simulação");  
@@ -32,6 +37,13 @@ public class Simulador extends ApplicationFrame{
 	   final ChartPanel chartPanel = new ChartPanel(chart);  
 	   chartPanel.setPreferredSize(new java.awt.Dimension (500, 270));  
 	   setContentPane(chartPanel);  
+	   
+	   File file = new File("./"+nomeGrafico+".png");
+       try {
+			ChartUtilities.saveChartAsPNG(file,chart,1920,1080);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	private static void plotarGrafico(XYSeries series, String nomeGrafico, String variavel)
@@ -45,7 +57,7 @@ public class Simulador extends ApplicationFrame{
 	
 	public static void main(String[] args){
 		execucao();
-		 
+		
 		
 		Scanner s = new Scanner(System.in);
 		while(true){
@@ -57,10 +69,10 @@ public class Simulador extends ApplicationFrame{
 					plotarGrafico(series, "custo x R4", "custo");
 					break;
 				case 2:
-					plotarGrafico(seriesCv, "cv x R4", "cv");		
+					plotarGrafico(seriesCv, "cv x R4", "(1.0 - pi_0) * cv");		
 					break;
 				case 3:
-					plotarGrafico(seriesCs, "cs x R4", "cs");
+					plotarGrafico(seriesCs, "cs x R4", "(pi_0 + pi_p) * r4 * cs");
 					break;
 				case 4:
 					plotarGrafico(seriesPi0, "Pi0 x R4", "Pi0");
@@ -89,10 +101,12 @@ public class Simulador extends ApplicationFrame{
 		});*/
 	}
 	
-	private static void execucao(){
+	private static void execucao()
+	{
 		Random rand = new Random();
+		
 		r4 = 0.0;
-		qtde = 20;
+		qtde = 100;
 		r1 = 2.0;
 		r2 = 0.8;
 		r3 = 3.0;
@@ -104,61 +118,77 @@ public class Simulador extends ApplicationFrame{
 		for(k=0;k<qtde;k++)
 		{
 			estado = '0';
-			t_0 = 0.0;
-			t_p = 0.0;
-			t_r = 0.0;
-			t_f = 0.0;
-			tempo = 0.0;
+			
 			media = 0.0;
 			variancia = 0.0;
+			double proxEvento = 0;
+			r4 += (1.0/qtde);
 			
 			System.out.println("\n========================== r4 = "+r4+" ==========================");
 			for(j=0; j<amostra;j++)
 			{
+				t_0 = 0.0;
+				t_p = 0.0;
+				t_r = 0.0;
+				t_f = 0.0;
+				tempo = 0.0;
 				for(i=0;i<iteracoes;i++)
 				{
-					random = rand.nextInt(1000);
+					random = rand.nextInt(100);
 					switch (estado) {
 						case '0':	
-							if(random > 800){
-								estado = 'p';
-							}
-							t_0 += 1.0;
+							estado='p';
+							proxEvento = gerador.geradorExponencial(r2);
+							tempo += proxEvento;
+							t_0 += proxEvento;
 							break;
 						case 'p':
-							if(random > 900){
+							double prob = (r4/(r4+lambda))*100;
+							
+							if(random < prob){
+								proxEvento = gerador.geradorExponencial(r4);
 								estado = 'r';
 							}
-							else if(random < amostra){
+							else if(random > prob){
 								estado = 'f';
+								proxEvento = gerador.geradorExponencial(lambda);
 							}
-							t_p += 1.0;
+							tempo += proxEvento;
+							t_p += proxEvento;
+							
 							break;
 						case 'r':
-							if(random > 100){
-								estado = '0';
-							}
-							t_r += 1.0;
+							estado='0';
+							proxEvento = gerador.geradorExponencial(r3);
+							tempo += proxEvento;
+							t_r += proxEvento;
+							//t_r += 1.0;
 							break;
 						case 'f':
-							if(random > 300){
-								estado = '0';
-							}
-							t_p += 1.0;
+							estado='0';
+							proxEvento = gerador.geradorExponencial(r1);
+							tempo += proxEvento;
+							t_f += proxEvento;
+							//t_p += 1.0;
 							break;
 						default:
 							System.out.println("Fail!");
 							System.exit(0);
 							break;
 					}
-					tempo += 1.0;
+					
+					//tempo += 1.0;
 				}
 				
 				pi_0 = t_0/tempo;
 				pi_p = t_p/tempo;
 				
-				custo[j] = (1.0 - pi_0) * cv + (pi_0 + pi_p) * r4 * cs;
+				custoNo[j] = ((1.0 - pi_0) * cv);
+				custoAmostra[j] = ((pi_0 + pi_p) * r4 * cs);
+				custo[j] = custoNo[j] + custoAmostra[j];
 				media += custo[j];
+				mediaCv += custoNo[j];
+				mediaCs += custoAmostra[j];
 				System.out.println("O custo foi de "+custo[j]);
 			}
 			
@@ -166,32 +196,66 @@ public class Simulador extends ApplicationFrame{
 			
 			System.out.println("\nO custo medio foi de "+ media);
 			
-			for(j=0;j<amostra;j++){
-				variancia += (custo[j]-media)*(custo[j]-media);
-			}
+			// variancia, desvio e intervalo de confianca para o custo
 			
-			variancia = (variancia / j);
-			
+			variancia = variancia(custo,media,amostra);
 			desvio = Math.sqrt(variancia);
 			
 			System.out.println("A variancia foi "+variancia+" e o desvio padrão foi de "+desvio);
 			
-			confiancaMenor = media - 1.96*(variancia/Math.sqrt(amostra));
-			confiancaMaior = media + 1.96*(variancia/Math.sqrt(amostra));
+			confiancaMenor = media - intervaloConfianca(desvio,amostra);
+			confiancaMaior = media + intervaloConfianca(desvio,amostra);
 			
 			System.out.println("O intervalo de confiança para 95% é: ("+confiancaMenor+"< u <"+confiancaMaior+")");
+
+			// variancia, desvio e intervalo de confianca para o custo do no CV
+			
+			variancia = variancia(custoNo,mediaCv,amostra);
+			desvio = Math.sqrt(variancia);
+			
+			System.out.println("A variancia foi "+variancia+" e o desvio padrão foi de "+desvio);
+			
+			confiancaMenor = mediaCv - intervaloConfianca(desvio,amostra);
+			confiancaMaior = mediaCv + intervaloConfianca(desvio,amostra);
+			
+			System.out.println("O intervalo de confiança para 95% é: ("+confiancaMenor+"< u <"+confiancaMaior+")");
+
+			// variancia, desvio e intervalo de confianca para o custo do no CS
+			
+			variancia = variancia(custoAmostra,mediaCs,amostra);
+			desvio = Math.sqrt(variancia);
+			
+			System.out.println("A variancia foi "+variancia+" e o desvio padrão foi de "+desvio);
+			
+			confiancaMenor = mediaCs - intervaloConfianca(desvio,amostra);
+			confiancaMaior = mediaCs + intervaloConfianca(desvio,amostra);
+			
+			System.out.println("O intervalo de confiança para 95% é: ("+confiancaMenor+"< u <"+confiancaMaior+")");
+
 			
 			series.add(r4, media);
 			seriesPi0.add(r4,pi_0);
-			seriesCs.add(r4,cs);
-			seriesCv.add(r4,cv);
+			seriesCs.add(r4,mediaCs);
+			seriesCv.add(r4,mediaCv);
 			
-			r4 += (1.0/qtde);
+			
 			System.out.println("");
 			
+		}	
+	}
+	
+	private static double intervaloConfianca(double desvio, int n)
+	{
+		return 1.96*(desvio/Math.sqrt(n));
+	}
+	
+	private static double variancia(double custo[], double media, int qtde)
+	{
+		double var = 0.0;
+		for(j=0;j<qtde;j++){
+			var += (custo[j]-media)*(custo[j]-media);
 		}
 		
-		
-		
+		return var/qtde;
 	}
 }
