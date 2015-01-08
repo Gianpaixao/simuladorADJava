57bcf4d412d81c9b14d6e563fa1f99c677f6b04b
Gian <gian.paixao@gmail.com>
2015-01-07 23:33:04 -0200

    Concertei o intervalo de confianca

--- SimuladorVirus/src/Simulador.java
+++ SimuladorVirus/src/Simulador.java
@@ -1,8 +1,5 @@
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.util.Random;
-
-import javax.swing.JComboBox;
+import java.util.Scanner;
 
 import org.jfree.chart.ChartFactory;
 import org.jfree.chart.ChartPanel;
@@ -15,32 +12,70 @@ import org.jfree.ui.RefineryUtilities;
 
 
 public class Simulador extends ApplicationFrame{
-	final static XYSeries series  = new XYSeries ("valor médio");;
+	final static XYSeries series  = new XYSeries ("custo médio");
+	final static XYSeries seriesPi0  = new XYSeries ("PI0");
+	final static XYSeries seriesCs  = new XYSeries ("CS");
+	final static XYSeries seriesCv  = new XYSeries ("CV");
 	static double r1, r2, r3, r4, lambda, pi_0, pi_p, pi_r, pi_f;
-	static double media = 0.0, variancia = 0.0, desvio, confianca;
+	static double media = 0.0, variancia = 0.0, desvio, confiancaMenor, confiancaMaior;
 	static char estado = '0';
 	static int i, j, k, qtde, random, amostra = 50, iteracoes = 50;
 	static double t_0 = 0.0, t_p = 0.0, t_r = 0.0, t_f = 0.0, tempo = 0.0, cv, cs, custo[] = new double[amostra];
 	
-	public Simulador(String title) {
-		super(title);  
- 
-       final XYSeriesCollection data = new XYSeriesCollection(series);  
-       final JFreeChart chart = ChartFactory.createXYLineChart("Custo X R4", "R4", "Custo",  
-           data,PlotOrientation.VERTICAL , true,true, false);  
+	public Simulador(XYSeries series, String nomeGrafico, String variavel) {
+		super("Grafico de Simulação");  
+		final JFreeChart chart;
+		final XYSeriesCollection data = new XYSeriesCollection(series);  
+	    chart = ChartFactory.createXYLineChart(nomeGrafico, "R4", variavel,  
+	           data,PlotOrientation.VERTICAL , true,true, false); 
+    
 	   final ChartPanel chartPanel = new ChartPanel(chart);  
 	   chartPanel.setPreferredSize(new java.awt.Dimension (500, 270));  
 	   setContentPane(chartPanel);  
 	}
 	
-	public static void main(String[] args){
-		execucao();
-		final Simulador demo = new Simulador("Grafico de Simulação");  
+	private static void plotarGrafico(XYSeries series, String nomeGrafico, String variavel)
+	{
+		final Simulador demo = new Simulador(series, nomeGrafico, variavel);  
 		demo.pack();  
 		RefineryUtilities.centerFrameOnScreen(demo);  
-		demo.setVisible(true); 
+		demo.setVisible(true);
+		demo.setDefaultCloseOperation(DO_NOTHING_ON_CLOSE);
+	}
+	
+	public static void main(String[] args){
+		execucao();
+		 
 		
-		String[] chartStrings = { "cv", "cs", "pi0", "custo", "pif" };
+		Scanner s = new Scanner(System.in);
+		while(true){
+			System.out.println("1 - custo\n2 - cv\n3 - cs\n4 - pi0\n5 - sair ");
+		    int grafico = s.nextInt();
+
+			switch (grafico) {
+				case 1:
+					plotarGrafico(series, "custo x R4", "custo");
+					break;
+				case 2:
+					plotarGrafico(seriesCv, "cv x R4", "cv");		
+					break;
+				case 3:
+					plotarGrafico(seriesCs, "cs x R4", "cs");
+					break;
+				case 4:
+					plotarGrafico(seriesPi0, "Pi0 x R4", "Pi0");
+					break;
+				case 5:
+					System.out.println("Fim da simulação2");
+					System.exit(0);
+					break;
+				default:
+					continue;
+			}
+			System.out.println("========================================\n");
+		}
+	    
+		/*String[] chartStrings = { "cv", "cs", "pi0", "custo", "pif" };
 
 		JComboBox chartList = new JComboBox(chartStrings);
 		chartList.setSelectedIndex(4);
@@ -51,7 +86,7 @@ public class Simulador extends ApplicationFrame{
 		        String chartName = (String)cb.getSelectedItem();
 		        if(chartName.equalsIgnoreCase("custo"));
 			}
-		});
+		});*/
 	}
 	
 	private static void execucao(){
@@ -141,11 +176,15 @@ public class Simulador extends ApplicationFrame{
 			
 			System.out.println("A variancia foi "+variancia+" e o desvio padrão foi de "+desvio);
 			
-			confianca = amostra / variancia;
+			confiancaMenor = media - 1.96*(variancia/Math.sqrt(amostra));
+			confiancaMaior = media + 1.96*(variancia/Math.sqrt(amostra));
 			
-			System.out.println("Confiança: "+confianca);
+			System.out.println("O intervalo de confiança para 95% é: ("+confiancaMenor+"< u <"+confiancaMaior+")");
 			
-			series.add(r4, media); 
+			series.add(r4, media);
+			seriesPi0.add(r4,pi_0);
+			seriesCs.add(r4,cs);
+			seriesCv.add(r4,cv);
 			
 			r4 += (1.0/qtde);
 			System.out.println("");
